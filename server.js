3cb39d05f029a5c52597df3a6ca16ea115a7a9b1
Ntyam Adjomo Francky Ludovic <francky.ntyam@gmail.com>
2024-05-19 03:05:32 +0100

    Update of week 03 activity:Error Handling

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
 const session = require("express-session")
 const pool = require("./database/")
+const utilities = require("./utilities/index")
 
 
 /************************************ 
@@ -48,20 +49,6 @@ app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at view root
 
 
-/**************************
- * Express Error Handler
- * Place after all other middleware
- ************************** */
-app.use(async (err, req,res,next) => {
-  let nav = await utilities.getNav()
-  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
-  res.render("errors/error", {
-    title: err.status || 'Server Error',
-    message: err.message,
-    nav
-  })
-})
-
 
 /* ***********************
  * Local Server Information
@@ -83,10 +70,11 @@ app.listen(port, () => {
 app.use(static)
 // index route
 
-app.get("/", baseController.buildHome)
+app.get("/", utilities.handleErrors(baseController.buildHome))
 
 //Inventory route
 app.use("/inv", inventoryRoute)
+
 
 //Account route
 
@@ -95,4 +83,19 @@ app.use("/inv", inventoryRoute)
 // file Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
   next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
+
+/**************************
+ * Express Error Handler
+ * Place after all other middleware
+ ************************** */
+app.use(async (err, req,res,next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else{message = 'Oh no! There was a crash. Maybe try a different route?'}
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
 })
--- utilities/index.js
+++ utilities/index.js
@@ -107,4 +107,11 @@ Util.buildInventoryGridDetail = async function(data_detail){
     return grid_detail
 }
 
+/*********************************
+ * Middleware For Handling Errors
+ * Wrap other function in this for
+ * General Error Handling
+ ********************************** */
+Util.handleErrors = fn => (req, res, next) => Promise.resolve(fn(req, res, next).catch(next))
+
 module.exports = Util
